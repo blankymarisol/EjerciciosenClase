f2fd7240d1777149949923c7f7737d7c3225be43
blanksss <marrophph@gmail.com>
2024-02-24 10:18:27 -0600

    primer ejercicio en clase

--- EjerciciosenClase/Program.cs
+++ EjerciciosenClase/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int x = 1;
+while (x <= 100)
+{
+    Console.WriteLine(x);
+    x++; //para que no se cree un bucle
+}
